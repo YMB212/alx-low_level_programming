86e09740a8c173788b69d1472f5591f08fcb7397
YMB212 <yohannismulugeta212@gmail.com>
2022-03-14 17:19:02 -0700

    printf and puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/*
- *this is comment 
- * */
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
